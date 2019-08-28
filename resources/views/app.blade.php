b49b236d0352b0326cb4b8b3a3612a0291f097f2
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-08-28 22:47:23 +0200

    Make installable

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,18 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <!--[if IE]><link rel="icon" type="image/png" href="/favicon.png"><![endif]-->
+    <title>Spotify</title>
+    <link rel="manifest" href="manifest.json">
+    <link rel="icon" type="image/png" sizes="196x196" href="/favicon.png">
+    <meta name="theme-color" content="#4DBA87">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
+    <meta name="apple-mobile-web-app-title" content="Spotify">
+    <link rel="apple-touch-icon" href="/favicon.png">
+    <link rel="mask-icon" href="/img/icons/safari-pinned-tab.svg" color=#38B2AC>
+    <meta name="msapplication-TileImage" content="/favicon.png">
+    <meta name="msapplication-TileColor" content="#ffffff">
     @routes
     <script type="text/javascript">
         if ('serviceWorker' in navigator) {
